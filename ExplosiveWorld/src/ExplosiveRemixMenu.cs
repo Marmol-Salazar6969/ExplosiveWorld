d001849d71f29d4a37cb5ecccb9e9f9dcae84169
BensoneWhite <75563186+Marmol-Salazar6969@users.noreply.github.com>
2023-05-12 21:18:58 -0700

    Explosive world v0.1
    Hey you are not supposed to see this!

---
+++ ExplosiveWorld/src/ExplosiveRemixMenu.cs
@@ -0,0 +1,98 @@
+﻿using Menu.Remix.MixedUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using RWCustom;
+
+namespace ExplosiveWorld
+{
+    public class ExplosiveMenu : OptionInterface
+    {
+        public static Configurable<bool> NoBombs { get; set; }
+        public static Configurable<bool> SingulatiryBombs { get; set; }
+
+        //public static Configurable<float> DamageSmall { get; set; }
+        //public static Configurable<float> DamageEXTREME { get; set; }
+
+        //public static Configurable<float> Rad { get; set; }
+        //public static Configurable<float> RadEXTREME { get; set; }
+
+        private OpCheckBox NoBombsBox;
+        private OpCheckBox SingulatiryBombsBox;
+
+        private OpLabel NoBombsLabel;
+        private OpLabel SingulatiryBombsLabel;
+
+        //Small select 1-100
+        // new OpUpdown(ScreenShakeValue, new Vector2(10,340), 50){_fMax = 10.0f, _fMin = 0.1f},
+        // new OpLabel(70f, 340f, "ScreenShake streght"),
+        //Big select
+        // new OpFloatSlider(MaulDamageExtra, new Vector2(5, 245), 200, 2, true){max = 10000f, min = 1f, hideLabel = false},
+        // new OpLabel(45f, 420f, "Maul Extra DAMAGE!"),
+
+        public ExplosiveMenu()
+        {
+            NoBombs = config.Bind("NoBombs", false, new ConfigAcceptableList<bool>(false, true));
+            SingulatiryBombs = config.Bind("SingulatiryBombs", false, new ConfigAcceptableList<bool>(false, true));
+        }
+        public override void Update()
+        {
+            base.Update();
+            Color colorOff = new Color(0.1451f, 0.1412f, 0.1529f);
+            Color colorOn = new Color(0.6627f, 0.6431f, 0.698f);
+
+            string caseValue = "";
+            if (NoBombsBox.value == "true") caseValue = "NoBombs";
+            else if (SingulatiryBombsBox.value == "true") caseValue = "SingulatiryBombsBox";
+
+            switch (caseValue)
+            {
+                case "NoBombs": //1
+                    NoBombsBox.greyedOut = false;
+                    SingulatiryBombsBox.greyedOut = true;
+
+                    NoBombsLabel.color = colorOn;
+                    SingulatiryBombsLabel.color = colorOff;
+                    break;
+
+                case "SingulatiryBombsBox": //2
+                    NoBombsBox.greyedOut = true;
+                    SingulatiryBombsBox.greyedOut = false;
+
+                    NoBombsLabel.color = colorOff;
+                    SingulatiryBombsLabel.color = colorOn;
+                    break;
+
+                default: //3
+                    NoBombsBox.greyedOut = false;
+                    SingulatiryBombsBox.greyedOut = false;
+
+                    NoBombsLabel.color = colorOn;
+                    SingulatiryBombsLabel.color = colorOn;
+                    break;
+            }
+        }
+
+        public override void Initialize()
+        {
+            var opTab1 = new OpTab(this, Translate("Options"));
+            Tabs = new[] { opTab1};
+
+            OpContainer tab1Container = new OpContainer(new Vector2(0, 0));
+            opTab1.AddItems(tab1Container);
+
+            UIelement[] UIArrayElements1 = new UIelement[]
+            {
+                NoBombsBox = new OpCheckBox(NoBombs, 10, 540) { description = Translate("Disable all the bombs") },
+                NoBombsLabel = new OpLabel(45f, 540f, Translate("No Bombs")),
+
+                SingulatiryBombsBox = new OpCheckBox(SingulatiryBombs, 10, 500) { description = Translate("Enable singularity bombs instead of normal bombs! :3c") },
+                SingulatiryBombsLabel = new OpLabel(45f, 500f, Translate("Singulatiry Bombs")),
+            };
+            opTab1.AddItems(UIArrayElements1);
+        }
+    }
+}
---
+++ ExplosiveWorld/src/ExplosiveWorld.cs
@@ -0,0 +1,375 @@
+﻿using BepInEx;
+using System;
+using UnityEngine;
+using Menu.Remix.MixedUI;
+using static Player;
+using Random = UnityEngine.Random;
+using MonoMod.Cil;
+using Mono.Cecil.Cil;
+using DevInterface;
+using System.Drawing;
+using Color = UnityEngine.Color;
+using static MonoMod.InlineRT.MonoModRule;
+using static MoreSlugcats.SingularityBomb;
+
+namespace ExplosiveWorld;
+[BepInPlugin("ExplosiveWorld", "Explosive World", "0.1")]
+
+public class ExplosiveWorld : BaseUnityPlugin {
+
+    static bool _initialized;
+
+    private readonly float bombDelay = 5f;
+    private float bombTime = 0f;
+
+    private readonly float bombPolelDelay = 0.6f;
+    private float bombPoleTimer = 0f;
+
+    private readonly float bombKelplDelay = 0.6f;
+    private float bombKelpTimer = 0f;
+
+    private bool bombInit = true;
+    private bool bombJumped = false;
+
+    private bool grabbombInit = true;
+    private bool grabbombInit2 = true;
+    private bool grabbombInit3 = true;
+
+    private bool bombSnail = true;
+
+    public Creature killTag;
+
+    public static ExplosiveMenu optionsMenuInstance;
+
+    private void LogInfo(object ex) => Logger.LogInfo(ex);
+
+    public void OnEnable() {
+        LogInfo("Explosive world is working!");
+        On.RainWorld.OnModsInit += RainWorld_OnModsInit;
+    }
+
+    private void RainWorld_OnModsInit(On.RainWorld.orig_OnModsInit orig, RainWorld self) {
+        orig(self);
+
+        try
+        {
+            if (_initialized) return;
+            _initialized = true;
+
+            On.Player.Update += Player_Update;
+
+            On.DropBug.Jump += DropBug_Jump;
+            On.DropBug.Update += DropBug_Update;
+
+            On.WaterNut.Swell += WaterNut_Swell;
+
+            On.Snail.Click += Snail_Click;
+
+            MachineConnector.SetRegisteredOI("ExplosiveWorld", optionsMenuInstance = new ExplosiveMenu());
+
+        }
+        catch (Exception ex)
+        {
+            Debug.Log($"Remix Menu: Hook_OnModsInit options failed init error {optionsMenuInstance}{ex}");
+            Logger.LogError(ex);
+            Logger.LogMessage("WHOOPS something go wrong");
+        }
+        finally
+        {
+            orig.Invoke(self);
+        }
+    }
+
+    private void Player_Update(On.Player.orig_Update orig, Player self, bool eu)
+    {
+        var room = self.room;
+        var pos = self.firstChunk.pos;
+        var color = self.ShortCutColor();
+
+        orig(self, eu);
+
+        if (self.dead || self.room is null || self is null)
+        {
+            bombInit = true;
+            grabbombInit = true;
+            grabbombInit2 = true;
+            grabbombInit3 = true;
+
+            bombJumped = false;
+
+            bombTime = 0f;
+            bombPoleTimer = 0f;
+            bombKelpTimer = 0f;
+        }
+
+        if (bombJumped && !self.dead && !ExplosiveMenu.NoBombs.Value)
+        {
+            bombTime += Time.deltaTime;
+        }
+
+        for (int dropbug = 0; dropbug < self.grabbedBy.Count; dropbug++)
+        {
+            if (self?.grabbedBy?[dropbug].grabber is DropBug && grabbombInit && !ExplosiveMenu.NoBombs.Value && !ExplosiveMenu.SingulatiryBombs.Value)
+            {
+                room?.AddObject(new Explosion(room, self, pos, 7, 250f, 6.2f, 2f, 280f, 0.25f, killTag, 0.7f, 160f, 1f));
+                room?.AddObject(new Explosion.ExplosionLight(pos, 280f, 1f, 7, color));
+                room?.AddObject(new Explosion.ExplosionLight(pos, 230f, 1f, 3, new Color(1f, 1f, 1f)));
+                room?.AddObject(new ExplosionSpikes(room, pos, 14, 30f, 9f, 7f, 170f, color));
+                room?.AddObject(new ShockWave(pos, 330f, 0.045f, 5, false));
+                room?.PlaySound(SoundID.Bomb_Explode, pos);
+
+                self.Die();
+                grabbombInit = false;
+            }
+
+            if (self?.grabbedBy?[dropbug].grabber is DropBug && grabbombInit && ExplosiveMenu.SingulatiryBombs.Value && !ExplosiveMenu.NoBombs.Value)
+            {
+                room?.AddObject(new SparkFlash(pos, 300f, new Color(0f, 0f, 1f)));
+                room?.AddObject(new Explosion(room, self, pos, 7, 250f, 6.2f, 200f, 280f, 0.25f, killTag, 0.7f, 160f, 1f));
+                room?.AddObject(new Explosion(room, self, pos, 7, 2000f, 4f, 220f, 400f, 0.25f, killTag, 0.3f, 200f, 1f));
+                room?.AddObject(new Explosion.ExplosionLight(pos, 280f, 1f, 7, color));
+                room?.AddObject(new Explosion.ExplosionLight(pos, 230f, 1f, 3, new Color(1f, 1f, 1f)));
+                room?.AddObject(new Explosion.ExplosionLight(pos, 2000f, 2f, 60, color));
+                room?.AddObject(new ShockWave(pos, 350f, 0.485f, 300, highLayer: true));
+                room?.AddObject(new ShockWave(pos, 2000f, 0.185f, 180));
+                room?.PlaySound(SoundID.Bomb_Explode, pos);
+                room?.InGameNoise(new Noise.InGameNoise(pos, 9000f, self, 1f));
+
+                self.Die();
+                grabbombInit = false;
+            }
+        }
+
+        for (int polemimic = 0; polemimic < self.grabbedBy.Count; polemimic++)
+        {
+            if (self?.grabbedBy?[polemimic].grabber is PoleMimic && !ExplosiveMenu.NoBombs.Value)
+            {
+                bombPoleTimer += Time.deltaTime;
+            }
+
+            if (self?.grabbedBy?[polemimic].grabber is PoleMimic && grabbombInit2 && bombPoleTimer >= bombPolelDelay && !ExplosiveMenu.NoBombs.Value && !ExplosiveMenu.SingulatiryBombs.Value)
+            {
+                room?.AddObject(new Explosion(room, self, pos, 7, 250f, 6.2f, 2f, 280f, 0.25f, killTag, 0.7f, 160f, 1f));
+                room?.AddObject(new Explosion.ExplosionLight(pos, 280f, 1f, 7, color));
+                room?.AddObject(new Explosion.ExplosionLight(pos, 230f, 1f, 3, new Color(1f, 1f, 1f)));
+                room?.AddObject(new ExplosionSpikes(room, pos, 14, 30f, 9f, 7f, 170f, color));
+                room?.AddObject(new ShockWave(pos, 330f, 0.045f, 5, false));
+                room?.PlaySound(SoundID.Bomb_Explode, pos);
+
+                self.Die();
+                grabbombInit2 = false;
+                bombPoleTimer = 0;
+            }
+
+            if (self?.grabbedBy?[polemimic].grabber is PoleMimic && grabbombInit2 && bombPoleTimer >= bombPolelDelay && ExplosiveMenu.SingulatiryBombs.Value && !ExplosiveMenu.NoBombs.Value)
+            {
+                room?.AddObject(new SparkFlash(pos, 300f, new Color(0f, 0f, 1f)));
+                room?.AddObject(new Explosion(room, self, pos, 7, 250f, 6.2f, 200f, 280f, 0.25f, killTag, 0.7f, 160f, 1f));
+                room?.AddObject(new Explosion(room, self, pos, 7, 2000f, 4f, 220f, 400f, 0.25f, killTag, 0.3f, 200f, 1f));
+                room?.AddObject(new Explosion.ExplosionLight(pos, 280f, 1f, 7, color));
+                room?.AddObject(new Explosion.ExplosionLight(pos, 230f, 1f, 3, new Color(1f, 1f, 1f)));
+                room?.AddObject(new Explosion.ExplosionLight(pos, 2000f, 2f, 60, color));
+                room?.AddObject(new ShockWave(pos, 350f, 0.485f, 300, highLayer: true));
+                room?.AddObject(new ShockWave(pos, 2000f, 0.185f, 180));
+                room?.PlaySound(SoundID.Bomb_Explode, pos);
+                room?.InGameNoise(new Noise.InGameNoise(pos, 9000f, self, 1f));
+
+                self.Die();
+                grabbombInit2 = false;
+                bombPoleTimer = 0;
+            }
+        }
+
+        for (int kelpmonster = 0; kelpmonster < self.grabbedBy.Count; kelpmonster++)
+        {
+            if (self?.grabbedBy?[kelpmonster].grabber is TentaclePlant && !ExplosiveMenu.NoBombs.Value)
+            {
+                bombKelpTimer += Time.deltaTime;
+            }
+
+            if (self?.grabbedBy?[kelpmonster].grabber is TentaclePlant && grabbombInit3 && bombKelpTimer >= bombKelplDelay && !ExplosiveMenu.NoBombs.Value && !ExplosiveMenu.SingulatiryBombs.Value)
+            {
+                room?.AddObject(new Explosion(room, self, pos, 7, 250f, 6.2f, 2f, 280f, 0.25f, killTag, 0.7f, 160f, 1f));
+                room?.AddObject(new Explosion.ExplosionLight(pos, 280f, 1f, 7, color));
+                room?.AddObject(new Explosion.ExplosionLight(pos, 230f, 1f, 3, new Color(1f, 1f, 1f)));
+                room?.AddObject(new ExplosionSpikes(room, pos, 14, 30f, 9f, 7f, 170f, color));
+                room?.AddObject(new ShockWave(pos, 330f, 0.045f, 5, false));
+                room?.PlaySound(SoundID.Bomb_Explode, pos);
+
+                self.Die();
+                grabbombInit3 = false;
+                bombKelpTimer = 0f;
+            }
+
+            if (self?.grabbedBy?[kelpmonster].grabber is TentaclePlant && grabbombInit3 && bombKelpTimer >= bombKelplDelay && ExplosiveMenu.SingulatiryBombs.Value && !ExplosiveMenu.NoBombs.Value)
+            {
+                room?.AddObject(new SparkFlash(pos, 300f, new Color(0f, 0f, 1f)));
+                room?.AddObject(new Explosion(room, self, pos, 7, 250f, 6.2f, 200f, 280f, 0.25f, killTag, 0.7f, 160f, 1f));
+                room?.AddObject(new Explosion(room, self, pos, 7, 2000f, 4f, 220f, 400f, 0.25f, killTag, 0.3f, 200f, 1f));
+                room?.AddObject(new Explosion.ExplosionLight(pos, 280f, 1f, 7, color));
+                room?.AddObject(new Explosion.ExplosionLight(pos, 230f, 1f, 3, new Color(1f, 1f, 1f)));
+                room?.AddObject(new Explosion.ExplosionLight(pos, 2000f, 2f, 60, color));
+                room?.AddObject(new ShockWave(pos, 350f, 0.485f, 300, highLayer: true));
+                room?.AddObject(new ShockWave(pos, 2000f, 0.185f, 180));
+                room?.PlaySound(SoundID.Bomb_Explode, pos);
+                room?.InGameNoise(new Noise.InGameNoise(pos, 9000f, self, 1f));
+
+                self.Die();
+                grabbombInit3 = false;
+                bombKelpTimer = 0f;
+            }
+        }
+    }
+
+    private void DropBug_Jump(On.DropBug.orig_Jump orig, DropBug self, Vector2 jumpDir)
+    {
+        var room = self.room;
+        var pos = self.firstChunk.pos;
+        var color = self.ShortCutColor();
+
+        orig(self, jumpDir);
+        if (self.jumping && bombInit && self.room is not null && self is not null && !self.dead && !ExplosiveMenu.NoBombs.Value && !ExplosiveMenu.SingulatiryBombs.Value)
+        {
+            room?.AddObject(new Explosion(room, self, pos, 7, 50f, 6.2f, 2f, 280f, 0.25f, killTag, 0.7f, 160f, 1f));
+            room?.AddObject(new Explosion.ExplosionLight(pos, 280f, 1f, 7, color));
+            room?.AddObject(new Explosion.ExplosionLight(pos, 230f, 1f, 3, new Color(1f, 1f, 1f)));
+            room?.AddObject(new ExplosionSpikes(room, pos, 14, 30f, 9f, 7f, 170f, color));
+            room?.AddObject(new ShockWave(pos, 330f, 0.045f, 5, false));
+            room?.PlaySound(SoundID.Bomb_Explode, pos);
+
+            bombInit = false;
+            bombJumped = true;
+        }
+
+        if (self.jumping && bombInit && self.room is not null && self is not null && !self.dead && ExplosiveMenu.SingulatiryBombs.Value && !ExplosiveMenu.NoBombs.Value)
+        {
+            room?.AddObject(new SparkFlash(pos, 300f, new Color(0f, 0f, 1f)));
+            room?.AddObject(new Explosion(room, self, pos, 7, 250f, 6.2f, 200f, 280f, 0.25f, killTag, 0.7f, 160f, 1f));
+            room?.AddObject(new Explosion(room, self, pos, 7, 2000f, 4f, 220f, 400f, 0.25f, killTag, 0.3f, 200f, 1f));
+            room?.AddObject(new Explosion.ExplosionLight(pos, 280f, 1f, 7, color));
+            room?.AddObject(new Explosion.ExplosionLight(pos, 230f, 1f, 3, new Color(1f, 1f, 1f)));
+            room?.AddObject(new Explosion.ExplosionLight(pos, 2000f, 2f, 60, color));
+            room?.AddObject(new ShockWave(pos, 350f, 0.485f, 300, highLayer: true));
+            room?.AddObject(new ShockWave(pos, 2000f, 0.185f, 180));
+            room?.PlaySound(SoundID.Bomb_Explode, pos);
+            room?.InGameNoise(new Noise.InGameNoise(pos, 9000f, self, 1f));
+
+            bombInit = false;
+            bombJumped = true;
+        }
+    }
+
+    private void DropBug_Update(On.DropBug.orig_Update orig, DropBug self, bool eu)
+    {
+        var room = self.room;
+        var pos = self.firstChunk.pos;
+        var color = self.ShortCutColor();
+
+        orig(self, eu);
+
+        if (bombTime >= bombDelay && !self.dead && !ExplosiveMenu.NoBombs.Value && !ExplosiveMenu.SingulatiryBombs.Value)
+        {
+            room?.AddObject(new Explosion(room, self, pos, 7, 50f, 6.2f, 2f, 280f, 0.25f, killTag, 0.7f, 160f, 1f));
+            room?.AddObject(new Explosion.ExplosionLight(pos, 280f, 1f, 7, color));
+            room?.AddObject(new Explosion.ExplosionLight(pos, 230f, 1f, 3, new Color(1f, 1f, 1f)));
+            room?.AddObject(new ExplosionSpikes(room, pos, 14, 30f, 9f, 7f, 170f, color));
+            room?.AddObject(new ShockWave(pos, 330f, 0.045f, 5, false));
+            room?.PlaySound(SoundID.Bomb_Explode, pos);
+
+            self.Die();
+            bombTime = 0f;
+            bombJumped = false;
+        }
+
+        if (bombTime >= bombDelay && !self.dead && bombSnail && ExplosiveMenu.SingulatiryBombs.Value && !ExplosiveMenu.NoBombs.Value)
+        {
+            room?.AddObject(new SparkFlash(pos, 300f, new Color(0f, 0f, 1f)));
+            room?.AddObject(new Explosion(room, self, pos, 7, 250f, 6.2f, 200f, 280f, 0.25f, killTag, 0.7f, 160f, 1f));
+            room?.AddObject(new Explosion(room, self, pos, 7, 2000f, 4f, 220f, 400f, 0.25f, killTag, 0.3f, 200f, 1f));
+            room?.AddObject(new Explosion.ExplosionLight(pos, 280f, 1f, 7, color));
+            room?.AddObject(new Explosion.ExplosionLight(pos, 230f, 1f, 3, new Color(1f, 1f, 1f)));
+            room?.AddObject(new Explosion.ExplosionLight(pos, 2000f, 2f, 60, color));
+            room?.AddObject(new ShockWave(pos, 350f, 0.485f, 300, highLayer: true));
+            room?.AddObject(new ShockWave(pos, 2000f, 0.185f, 180));
+            room?.PlaySound(SoundID.Bomb_Explode, pos);
+            room?.InGameNoise(new Noise.InGameNoise(pos, 9000f, self, 1f));
+
+            self.Die();
+            bombTime = 0f;
+            bombJumped = false;
+        }
+    }
+
+    private void Snail_Click(On.Snail.orig_Click orig, Snail self)
+    {
+        var room = self.room;
+        var pos = self.firstChunk.pos;
+        var color = self.ShortCutColor();
+
+        orig(self);
+        if (self.triggered)
+        {
+            bombSnail = true;
+        }
+
+        if (!self.triggered && self.room is not null && self is not null && bombSnail && !ExplosiveMenu.NoBombs.Value && !ExplosiveMenu.SingulatiryBombs.Value)
+        {
+            room?.AddObject(new Explosion(room, self, pos, 7, 250f, 6.2f, 2f, 280f, 0.25f, killTag, 0.7f, 160f, 1f));
+            room?.AddObject(new Explosion.ExplosionLight(pos, 280f, 1f, 7, color));
+            room?.AddObject(new Explosion.ExplosionLight(pos, 230f, 1f, 3, new Color(1f, 1f, 1f)));
+            room?.AddObject(new ExplosionSpikes(room, pos, 14, 30f, 9f, 7f, 170f, color));
+            room?.AddObject(new ShockWave(pos, 330f, 0.045f, 5, false));
+            room?.PlaySound(SoundID.Bomb_Explode, pos);
+
+            bombSnail = false;
+        }
+
+        if (!self.triggered && self.room is not null && self is not null && bombSnail && ExplosiveMenu.SingulatiryBombs.Value && !ExplosiveMenu.NoBombs.Value)
+        {
+            room?.AddObject(new SparkFlash(pos, 300f, new Color(0f, 0f, 1f)));
+            room?.AddObject(new Explosion(room, self, pos, 7, 250f, 6.2f, 200f, 280f, 0.25f, killTag, 0.7f, 160f, 1f));
+            room?.AddObject(new Explosion(room, self, pos, 7, 2000f, 4f, 220f, 400f, 0.25f, killTag, 0.3f, 200f, 1f));
+            room?.AddObject(new Explosion.ExplosionLight(pos, 280f, 1f, 7, color));
+            room?.AddObject(new Explosion.ExplosionLight(pos, 230f, 1f, 3, new Color(1f, 1f, 1f)));
+            room?.AddObject(new Explosion.ExplosionLight(pos, 2000f, 2f, 60, color));
+            room?.AddObject(new ShockWave(pos, 350f, 0.485f, 300, highLayer: true));
+            room?.AddObject(new ShockWave(pos, 2000f, 0.185f, 180));
+            room?.PlaySound(SoundID.Bomb_Explode, pos);
+            room?.InGameNoise(new Noise.InGameNoise(pos, 9000f, self, 1f));
+
+            bombSnail = false;
+        }
+    } 
+
+    private void WaterNut_Swell(On.WaterNut.orig_Swell orig, WaterNut self)
+    {
+        var room = self.room;
+        var pos = self.firstChunk.pos;
+        var color = new Color(1f, 1f, 1f);
+
+        orig(self);
+        if (self.AbstrNut.swollen && self.room is not null && self is not null && !ExplosiveMenu.NoBombs.Value && !ExplosiveMenu.SingulatiryBombs.Value)
+        {
+            room?.AddObject(new Explosion(room, self, pos, 7, 250f, 6.2f, 2f, 280f, 0.25f, killTag, 0.7f, 160f, 1f));
+            room?.AddObject(new Explosion.ExplosionLight(pos, 280f, 1f, 7, color));
+            room?.AddObject(new Explosion.ExplosionLight(pos, 230f, 1f, 3, new Color(1f, 1f, 1f)));
+            room?.AddObject(new ExplosionSpikes(room, pos, 14, 30f, 9f, 7f, 170f, color));
+            room?.AddObject(new ShockWave(pos, 330f, 0.045f, 5, false));
+            room?.PlaySound(SoundID.Bomb_Explode, pos);
+        }
+
+        if (self.AbstrNut.swollen && self.room is not null && self is not null && ExplosiveMenu.SingulatiryBombs.Value && !ExplosiveMenu.NoBombs.Value)
+        {
+            room?.AddObject(new SparkFlash(pos, 300f, new Color(0f, 0f, 1f)));
+            room?.AddObject(new Explosion(room, self, pos, 7, 250f, 6.2f, 200f, 280f, 0.25f, killTag, 0.7f, 160f, 1f));
+            room?.AddObject(new Explosion(room, self, pos, 7, 2000f, 4f, 220f, 400f, 0.25f, killTag, 0.3f, 200f, 1f));
+            room?.AddObject(new Explosion.ExplosionLight(pos, 280f, 1f, 7, color));
+            room?.AddObject(new Explosion.ExplosionLight(pos, 230f, 1f, 3, new Color(1f, 1f, 1f)));
+            room?.AddObject(new Explosion.ExplosionLight(pos, 2000f, 2f, 60, color));
+            room?.AddObject(new ShockWave(pos, 350f, 0.485f, 300, highLayer: true));
+            room?.AddObject(new ShockWave(pos, 2000f, 0.185f, 180));
+            room?.PlaySound(SoundID.Bomb_Explode, pos);
+            room?.InGameNoise(new Noise.InGameNoise(pos, 9000f, self, 1f));
+        }    
+    }
+}
